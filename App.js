fb4f5ff979cd84362a60b5c5aaa7e97f610f70fd
SayaniSen2022 <sayanisen.official@gmail.com>
2023-09-10 16:58:07 +0530

    ep04

--- App.js
+++ App.js
@@ -1,16 +1,89 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => <h1 id="heading">Namaste React JSX</h1>; //React element
+/**
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Search
+ *  - RestaurantContainer
+ *      - RestaurantCard
+ *        - Image
+ *        - Star rating, name of res, cuisine, time for delivery
+ * Footer
+ *  - Links
+ *  - Copyright
+ *  - Contact
+ */
 
-const HeadingComponent = () => (
-  <div id="container">
-    <Title />
-    <Title></Title>
-    {Title()}
-    <h1>Namaste React FC</h1>
-  </div>
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiJ7CBZpGO0w58fuzfGwZf0sPkfK-KIAI_56W5-SKN&s"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
+const styleResCard = {
+  backgroundColor: "#f0f0f0",
+};
+
+const RestaurantCard = (props) => {
+  return (
+    <div className="res-card" style={styleResCard}>
+      <img
+        src="https://media.istockphoto.com/id/488481490/photo/fish-biryani-with-basmati-rice-indian-food.jpg?s=612x612&w=0&k=20&c=9xEw3VOQSz9TP8yQr60L47uExyKF9kogRhQdlghlC00="
+        alt="res-logo"
+        className="res-logo"
+      />
+      <h3>{props.resName}</h3>
+      <h4>{props.cuisine}</h4>
+      <h4>{props.rating}</h4>
+      <h4>38 mins</h4>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard
+          resName={"Kolkata 34"}
+          cuisine={"Biriyani, North Inidian"}
+          rating="4.4 stars"
+        />
+        <RestaurantCard
+          resName={"Oudh 1590"}
+          cuisine={"Awadhi, North Inidian"}
+          rating="4.2 stars"
+        />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
---
+++ Assignments/Assignment03.md
@@ -0,0 +1,14 @@
+# What is JSX?
+
+- JSX is a JS syntax extension which enables us to write HTML-like code inside Javascript functions. It is independent of React but was developed by Facebook developers for use with React.
+
+# Superpowers of JSX
+
+- We can embed JS expressions inside JSX using {}.
+- JSX makes it easier to write declarative code as we are combining power of JS with familiar syntax of HTML. Makes it easier to define the structure and appearance of components.
+- More understandable and readable, increases maintainability.
+
+# Role of type attribute in script tag? What options can I use there?
+
+- The type attribute specifies the type of the script.
+- Values: application/javascript (default) or application/ecmascript; module; src
