71cb2cdd0dd73df536a7a3aadeaaf08a364d7f87
dcpoptest <dcpoptest@pop-os.localdomain>
2023-10-13 16:38:19 +0530

    ep09

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () => {
   //state variable
@@ -40,6 +41,14 @@ const Body = () => {
     setFilteredRestaurant(filteredRestaurant);
     setSearchText("");
   };
+
+  const onlineStatus = useOnlineStatus();
+  if (onlineStatus === false)
+    return (
+      <h1>
+        Looks like you are offline. Please check your internet connection.
+      </h1>
+    );
 
   //conditional rendering
   return listOfRestaurants.length === 0 ? (
